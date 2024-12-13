d2106905d3e3dfaf372c19c7bc070caf99b82da1
KoroleniaIlia <korolenya.ilya2017@gmail.com>
2024-12-13 21:42:32 +0200

    Initial commit

---
+++ src/main/java/com/example/lab_java_web/LabJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_java_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJavaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabJavaWebApplication.class, args);
+    }
+
+}
